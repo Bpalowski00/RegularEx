7cc3c2703f4fc6dcb36a485b6dde007aebd9d757
b.palowski <brianpalowski00@gmail.com>
2019-02-17 18:51:12 -0800

    Practice Regular expressions

---
+++ app.js
@@ -0,0 +1,25 @@
+// let re;
+// re = /hello/;
+// re = /hello/i;// upper lower
+// re = /hello/g;// checks global
+
+// console.log(re);
+// console.log(re.source);
+
+// const result = re.exec('hello world');
+
+// console.log(result);
+// console.log(result[0]);
+// console.log(result.index);
+// console.log(result.input);
+
+// test() - Returns true or false
+// const result = re.test('hello');
+// console.log(result);
+
+// match()- returns result array or null
+// const str = 'hello world';
+
+// const result = str.match(re);
+// console.log(result);
+
